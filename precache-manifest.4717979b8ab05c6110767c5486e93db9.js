32f507fc3871f675c13f8aa09c02121e9fe07732
Akash <akashsuper2000@gmail.com>
2019-11-29 19:13:28 +0530

    Updates

---
+++ precache-manifest.4717979b8ab05c6110767c5486e93db9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df74c31088fa81e05a050f741c194e7a",
+    "url": "/faculty-dashboard/index.html"
+  },
+  {
+    "revision": "423ea58a2fb1f3981c82",
+    "url": "/faculty-dashboard/static/css/main.ac544417.chunk.css"
+  },
+  {
+    "revision": "68f4f36da4c6af596758",
+    "url": "/faculty-dashboard/static/js/2.ad4e31db.chunk.js"
+  },
+  {
+    "revision": "423ea58a2fb1f3981c82",
+    "url": "/faculty-dashboard/static/js/main.91d53acd.chunk.js"
+  },
+  {
+    "revision": "3fb3a872803882618d21",
+    "url": "/faculty-dashboard/static/js/runtime-main.3f64ebb8.js"
+  }
+]);
