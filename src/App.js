abd90c6434405c357eb39b046d9a47925622eca2
Akash <akashsuper2000@gmail.com>
2019-11-29 18:41:37 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
+import './bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      user: 'admin',
+      pass: 'password',
+    }
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  render(){
+    return(
+
+      <div className='Container'>
+      <div className='Box'>
+        <Login />
+      </div>
+      </div>
+
+    );
+  }
 }
+
+
+
+class Login extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      user: 'admin',
+      pass: 'password',
+    }
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  render(){
+    return(
+      <div>
+      <h1 className='Heads'>Login</h1>
+      <div className='Content'>
+      <form>
+
+      <div className='FormGroup'>
+      <input className='InputBoxes' name='username' placeholder='Name'></input>
+      <span className='Subheads' name='userinv' style={{visibility: 'collapse'}}>Enter a username!</span>
+      </div>
+
+      <div className='FormGroup'>
+      <input className='InputBoxes' name='password' type='password' placeholder='Password'></input>
+      <span className='Subheads' name='passinv' style={{visibility: 'collapse'}}>Invalid password!</span>
+      </div>
+
+      </form>       
+          
+      </div>
+      </div>
+
+    );
+  }
+}
+
 
 export default App;
