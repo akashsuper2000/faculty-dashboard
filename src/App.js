88ca34f5551533e308c06b00ca598eca40f1cace
Akash <akashsuper2000@gmail.com>
2019-11-29 19:17:17 +0530

    github pages

--- src/App.js
+++ src/App.js
@@ -22,49 +22,23 @@ class App extends React.Component{
 
       <div className='Container'>
       <div className='Box'>
-        <Login />
-      </div>
-      </div>
+        <h1 className='Heads'>Login</h1>
+        <div className='Content'>
+        <form>
 
-    );
-  }
-}
+        <div className='FormGroup'>
+        <input className='InputBoxes' name='username' placeholder='Name'></input>
+        <span className='Subheads' name='userinv' style={{visibility: 'collapse'}}>Enter a username!</span>
+        </div>
 
+        <div className='FormGroup'>
+        <input className='InputBoxes' name='password' type='password' placeholder='Password'></input>
+        <span className='Subheads' name='passinv' style={{visibility: 'collapse'}}>Invalid password!</span>
+        </div>
 
-
-class Login extends React.Component{
-
-  constructor(props){
-    super(props);
-    this.state = {
-      user: 'admin',
-      pass: 'password',
-    }
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-  }
-
-  render(){
-    return(
-      <div>
-      <h1 className='Heads'>Login</h1>
-      <div className='Content'>
-      <form>
-
-      <div className='FormGroup'>
-      <input className='InputBoxes' name='username' placeholder='Name'></input>
-      <span className='Subheads' name='userinv' style={{visibility: 'collapse'}}>Enter a username!</span>
-      </div>
-
-      <div className='FormGroup'>
-      <input className='InputBoxes' name='password' type='password' placeholder='Password'></input>
-      <span className='Subheads' name='passinv' style={{visibility: 'collapse'}}>Invalid password!</span>
-      </div>
-
-      </form>       
-          
+        </form>       
+            
+        </div>
       </div>
       </div>
 
