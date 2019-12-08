9a16304d423c8944d985d7677b404660bdc93713
Akash <akashsuper2000@gmail.com>
2019-12-08 22:28:48 +0530

    Added auth pages

--- src/App.js
+++ src/App.js
@@ -1,48 +1,192 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
+import ReactPasswordStrength from 'react-password-strength';
 import './bootstrap.css';
+import './App.css';
 
 class App extends React.Component{
 
   constructor(props){
     super(props);
     this.state = {
-      user: 'admin',
-      pass: 'password',
+      page: 0,
     }
   };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
+  handleLogin = (e) => {
+    e.preventDefault();
+    var data = {user: document.getElementsByName('username')[0].value,
+    cost: document.getElementsByName('password')[0].value};
+
+    console.log(JSON.stringify(data));
+    fetch("https://us-central1-nodejs-workshop-210811.cloudfunctions.net/mysql-function-2/bill", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+        body: JSON.stringify(data)
+    }).then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+    }).then(function(data) {
+        console.log(data);  
+        // eslint-disable-next-line
+        if(data == "success"){
+        console.log('Success'); 
+        }
+    }).catch(function(err) {
+        console.log(err);
+    });
+  }
+
+  handleRegister = (e) => {
+    e.preventDefault();
+    var data = {user: document.getElementsByName('username')[0].value,
+    email: document.getElementsByName('email')[0].value,
+    cost: document.getElementsByName('password')[0].value};
+    
+    console.log(JSON.stringify(data));
+    fetch("https://us-central1-nodejs-workshop-210811.cloudfunctions.net/mysql-function-2/bill", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+        body: JSON.stringify(data)
+    }).then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+    }).then(function(data) {
+        console.log(data);  
+        // eslint-disable-next-line
+        if(data == "success"){
+        console.log('Success'); 
+        }
+    }).catch(function(err) {
+        console.log(err);
+    });
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    if(e.target.value=='Register'){
+      this.setState({page: 1});
+    }
+    else if(e.target.value=='Forgot Password?'){
+      this.setState({page: 2});
+    }
+    else{
+      this.setState({page: 0});
+    }
   }
 
   render(){
-    return(
 
-      <div className='Container'>
-      <div className='Box'>
-        <h1 className='Heads'>Login</h1>
-        <div className='Content'>
-        <form>
+    if(this.state.page==0){
+      return(
 
-        <div className='FormGroup'>
-        <input className='InputBoxes' name='username' placeholder='Name'></input>
-        <span className='Subheads' name='userinv' style={{visibility: 'collapse'}}>Enter a username!</span>
+        <div className='Container'>
+        <div className='Box'>
+          <h1 className='Heads'>Sign In</h1>
+          <div className='Content'>
+          <form onSubmit={this.handleLogin}>
+
+          <div className='FormGroup'>
+          <input className='InputBoxes' name='username' placeholder='Name' required></input>
+          <span className='Subheads' name='userinv' style={{visibility: 'collapse'}}>Enter a username!</span>
+          </div>
+
+          <div className='FormGroup'>
+          <input className='InputBoxes' name='password' type='password' placeholder='Password' required></input>
+          <span className='Subheads' name='passinv' style={{visibility: 'collapse'}}>Invalid password!</span>
+          </div>
+
+          <input className='btn-lg btn btn-default' type='submit' value='Login'/><br/>
+          <div className='FormGroup'>
+          <input className='btn-md btn btn-default Smbtn' value='Register' onClick={this.handleChange}/>
+          <input className='btn-md btn btn-default Smbtn' value='Forgot Password?' onClick={this.handleChange}/>
+          </div>
+          </form>       
+              
+          </div>
+        </div>
         </div>
 
-        <div className='FormGroup'>
-        <input className='InputBoxes' name='password' type='password' placeholder='Password'></input>
-        <span className='Subheads' name='passinv' style={{visibility: 'collapse'}}>Invalid password!</span>
+      );
+    }
+
+    else if(this.state.page==1){
+      return(
+
+        <div className='Container'>
+        <div className='Box'>
+          <h1 className='Heads'>Register</h1>
+          <div className='Content'>
+          <form onSubmit={this.handleRegister}>
+
+          <div className='FormGroup'>
+          <input className='InputBoxes' name='username' placeholder='Name' required></input>
+          <span className='Subheads' name='userinv' style={{visibility: 'collapse'}}>Enter a username!</span>
+          </div>
+
+          <div className='FormGroup'>
+          <input className='InputBoxes' name='email' placeholder='Email ID' pattern='^[a-zA-Z0-9]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+$' required></input>
+          <span className='Subheads' name='userinv' style={{visibility: 'collapse'}}>Enter an Email!</span>
+          </div>
+
+          <div className='FormGroup'>
+          <ReactPasswordStrength
+            className="FormGroup"
+            style={{border: 0}}
+            minLength={5}
+            minScore={2}
+            scoreWords={['Weak', 'Okay', 'Good', 'Strong', 'Stronger']}
+            tooShortWord={'Too short'}
+            inputProps={{ name: "password", autoComplete: "off", className: "Passbox", placeholder: "Password", required: 'true', pattern: '^.{5}.*$' }}
+          />
+          </div>
+
+          <input className='btn-lg btn btn-default' type='submit' value='Register'/><br/>
+          <div className='FormGroup'>
+          <input className='btn-md btn btn-default Smbtn' value='Login' onClick={this.handleChange}/>
+          <input className='btn-md btn btn-default Smbtn' value='Forgot Password?' onClick={this.handleChange}/>
+          </div>
+          </form>       
+              
+          </div>
+        </div>
         </div>
 
-        </form>       
-            
-        </div>
-      </div>
-      </div>
+      );
+    }
 
-    );
+    else{
+      return(
+
+        <div className='Container'>
+        <div className='Box'>
+          <h1 className='Heads' style={{fontSize: 50}}>Forgot Password</h1>
+          <div className='Content'>
+          <form onSubmit={this.handleFP}>
+
+          <div className='FormGroup'>
+          <span className='Subheads' name='otp-thing'>Enter the OTP received in your mail ID</span>
+          <input className='InputBoxes' style={{margin: 50}} name='otp' placeholder='OTP' type='number' required></input>
+          </div>
+
+          <input className='btn-lg btn btn-default' type='submit' value='Submit'/><br/>
+          <div className='FormGroup'>
+          <input className='btn-md btn btn-default Smbtn' value='Login' onClick={this.handleChange}/>
+          <input className='btn-md btn btn-default Smbtn' value='Register' onClick={this.handleChange}/>
+          </div>
+          </form>       
+              
+          </div>
+        </div>
+        </div>
+
+      );
+    }
   }
 }
 
