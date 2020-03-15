728499bbc117008b557063ac9bd8c082b19dc356
himanshu2602 <53013138+himanshu2602@users.noreply.github.com>
2020-03-16 01:24:29 +0530

    Update inserter_db.sql

--- sql/inserter_db.sql
+++ sql/inserter_db.sql
@@ -10,11 +10,15 @@ INSERT INTO `users`.`users`  VALUES ('cb.en.cse120', 'Dr. Thangavelu S.', 'cse',
 INSERT INTO `users`.`users`  VALUES ('sample', 'akash', 'cse', 'sefacultydashboard@gmail.com', 'password');
 
 
+
 INSERT INTO `users`.`leavelog` VALUES ('1', 'cb.en.cse044', 'Casual Leave', '2020-04-12', '2020-05-15', 'Family Emergency', 'Pending');
 INSERT INTO `users`.`leavelog` VALUES ('2', 'cb.en.cse046', 'Sick Leave', '2020-05-15', '2020-07-01', 'Hand Fracture', 'Approved');
 INSERT INTO `users`.`leavelog` VALUES ('3', 'cb.en.cse120', 'Privilege Leave', '2020-04-24', '2020-06-18', 'Family Tour', 'Declined');
 INSERT INTO `users`.`leavelog` VALUES ('4', 'cb.en.cse044', 'Sick Leave', '2020-05-12', '2020-07-15', 'Lung Operation', 'Approved');
 INSERT INTO `users`.`leavelog` VALUES ('5', 'cb.en.cse046', 'Privilege Leave', '2020-06-15', '2020-08-15', 'Going out of town', 'Pending');
+INSERT INTO `users`.`leavelog` VALUES ('6', 'cb.en.cse044', 'Casual Leave', '2020-06-12', '2020-08-15', 'Family Issues', 'Pending');
+INSERT INTO `users`.`leavelog` VALUES ('7', 'cb.en.cse044', 'Casual Leave', '2020-05-12', '2020-05-18', 'Tour with family', 'Pending');
+INSERT INTO `users`.`leavelog` VALUES ('8', 'cb.en.cse044', 'Casual Leave', '2020-07-12', '2020-07-18', 'Going out of Town', 'Declined');
 
 INSERT INTO `users`.`announcement` VALUES ('1','cse','2020-03-06','Cyber Security Seminar on coming 21st March');
 INSERT INTO `users`.`announcement` VALUES ('2','cse','2020-02-28','Industry Interaction on coming 15th March');
