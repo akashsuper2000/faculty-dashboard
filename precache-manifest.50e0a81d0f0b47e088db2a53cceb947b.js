9d7d1fe77b5ae19346a4c5d419320ef05b9de4f0
akashsuper2000 <akashsuper2000@gmail.com>
2020-03-16 05:16:25 +0000

    Deploying to gh-pages from  - a7a4f53ad07f9eba8d210f5628d8c282a20d80e4 🚀

---
+++ precache-manifest.50e0a81d0f0b47e088db2a53cceb947b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a4dce42d955f0f878f61b92d94c040",
+    "url": "/faculty-dashboard/index.html"
+  },
+  {
+    "revision": "d41f82ad7e98908f182c",
+    "url": "/faculty-dashboard/static/css/2.fa71fc11.chunk.css"
+  },
+  {
+    "revision": "1206cbc87ec98eba6d33",
+    "url": "/faculty-dashboard/static/css/main.bdf73bd8.chunk.css"
+  },
+  {
+    "revision": "d41f82ad7e98908f182c",
+    "url": "/faculty-dashboard/static/js/2.9e5ee50b.chunk.js"
+  },
+  {
+    "revision": "1206cbc87ec98eba6d33",
+    "url": "/faculty-dashboard/static/js/main.0e7ca08c.chunk.js"
+  },
+  {
+    "revision": "16c0e1d8fcffd8c07078",
+    "url": "/faculty-dashboard/static/js/runtime-main.62cee297.js"
+  },
+  {
+    "revision": "e9b8d5e79ecdb5e55225aacd78a59386",
+    "url": "/faculty-dashboard/static/media/univlogo.e9b8d5e7.svg"
+  }
+]);
